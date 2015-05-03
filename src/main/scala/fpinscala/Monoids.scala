44993e66ff05572d4a1442bea3ccdb661a6fe2fb
stevejb71 <stevejb71@yahoo.co.uk>
2015-05-03 16:41:05 +0800

    Exercise 10.12,10.13

--- src/main/scala/fpinscala/Monoids.scala
+++ src/main/scala/fpinscala/Monoids.scala
@@ -99,7 +99,7 @@ object Monoids {
 
   val wcMonoid  = new Monoid[WC] {
     override def op(wc1: WC, wc2: WC) = (wc1,wc2) match {
-      case (Stub(c1),Stub(c2)) => Stub(c1 + c2)
+      case (Stub(c1),Stub(c2)) => Stub(c1+c2)
       case (Stub(s),Part(l,w,r)) => Part(s+l,w,r)
       case (Part(l,w,r),Stub(s)) => Part(l,w,r+s)
       case (Part(l1,w1,r1),Part(l2,w2,r2)) => Part(l1,w1+w2+1,r2)
@@ -120,11 +120,11 @@ object Monoids {
   /* -------- Exercise 10.12 -------- */
 
   trait Foldable[F[_]] {
-    def foldRight[A, B](as: F[A])(z: B)(f: (A, B) => B): B
-    def foldLeft[A, B](as: F[A])(z: B)(f: (B, A) => B): B
+    def foldRight[A, B](as: F[A])(z: B)(f: (A, B) => B): B = foldMap(as)((a:A) => (b:B) => f(a,b))(endoMonoid[B])(z)
+    def foldLeft[A, B](as: F[A])(z: B)(f: (B, A) => B): B = foldMap(as)((a:A) => (b:B) => f(b,a))(dual(endoMonoid[B]))(z)
     def foldMap[A, B](as: F[A])(f: A => B)(mb: Monoid[B]): B
     def concatenate[A](as: F[A])(m: Monoid[A]): A = foldLeft(as)(m.zero)(m.op)
-    def toList[A](as: F[A]): List[A]
+    def toList[A](as: F[A]): List[A] = foldRight(as)(Nil:List[A])(_ :: _)
   }
 
   object ListFoldable extends Foldable[List] {
@@ -134,5 +134,31 @@ object Monoids {
     override def foldMap[A, B](as: List[A])(f: A => B)(mb: Monoid[B]): B = foldLeft(as)(mb.zero)((b,a) => mb.op(b,f(a)))
   }
 
+  object IndexedSeqFoldable extends Foldable[IndexedSeq] {
+    override def foldRight[A, B](as: IndexedSeq[A])(z: B)(f: (A, B) => B): B = as.foldRight(z)(f)
+    override def foldLeft[A, B](as: IndexedSeq[A])(z: B)(f: (B, A) => B): B = as.foldLeft(z)(f)
+    override def toList[A](as: IndexedSeq[A]): List[A] = as.toList
+    override def foldMap[A, B](as: IndexedSeq[A])(f: A => B)(mb: Monoid[B]): B = foldMapV(as,mb)(f)
+  }
 
+  object StreamFoldable extends Foldable[Stream] {
+    override def foldRight[A, B](as: Stream[A])(z: B)(f: (A, B) => B): B = as.foldRight(z)(f)
+    override def foldLeft[A, B](as: Stream[A])(z: B)(f: (B, A) => B): B = as.foldLeft(z)(f)
+    override def toList[A](as: Stream[A]): List[A] = as.toList
+    override def foldMap[A, B](as: Stream[A])(f: A => B)(mb: Monoid[B]): B = concatenate(as map f)(mb)
+  }
+
+  /* -------- Exercise 10.13 -------- */
+
+  sealed trait Tree[+A]
+  case class Leaf[A](value: A) extends Tree[A]
+  case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+  object TreeFoldable extends Foldable[Tree] {
+    override def foldMap[A, B](as: Tree[A])(f: A => B)(mb: Monoid[B]): B = as match {
+      case Leaf(a) => f(a)
+      case Branch(l,r) => mb.op(foldMap(l)(f)(mb),foldMap(r)(f)(mb))
+    }
+    // foldLeft / Right / toList defaulted so inefficient
+  }
 }
