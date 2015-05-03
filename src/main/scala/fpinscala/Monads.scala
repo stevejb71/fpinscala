7da41cc5e1dabc231ea23e25c0b60b445d204ca9
stevejb71 <stevejb71@yahoo.co.uk>
2015-05-03 22:10:09 +0800

    Monads - Exercise 11.3

--- src/main/scala/fpinscala/Monads.scala
+++ src/main/scala/fpinscala/Monads.scala
@@ -9,6 +9,9 @@ trait Monad[F[_]] extends Functor[F] {
   def flatMap[A,B](ma: F[A])(f: A => F[B]): F[B]
   def map[A,B](ma: F[A])(f: A => B): F[B] = flatMap(ma)(a => unit(f(a)))
   def map2[A,B,C](ma: F[A], mb: F[B])(f: (A,B) => C): F[C] = flatMap(ma)(a => map(mb)(f(a,_)))
+  /* -------- Exercise 11.3 ------- */
+  def sequence[A](lma: List[F[A]]): F[List[A]] = lma.foldLeft(unit(Nil:List[A]))((acc,b) => map2(b,acc)(_ :: _))
+  def traverse[A,B](la: List[A])(f: A => F[B]): F[List[B]] = sequence(la map f)
 }
 
 object Monads {
