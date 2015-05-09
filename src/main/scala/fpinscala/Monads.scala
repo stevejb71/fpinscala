7477c42d4e6c2cf9c8a20e520884510d7cb6f145
stevejb71 <stevejb71@yahoo.co.uk>
2015-05-09 14:51:53 +0800

    Exercise 11.8

--- src/main/scala/fpinscala/Monads.scala
+++ src/main/scala/fpinscala/Monads.scala
@@ -8,7 +8,8 @@ trait Functor[F[_]] {
 
 trait Monad[F[_]] extends Functor[F] {
   def unit[A](a: => A): F[A]
-  def flatMap[A, B](ma: F[A])(f: A => F[B]): F[B]
+  /* -------- Exercise 11.8 ------- */
+  def flatMap[A, B](ma: F[A])(f: A => F[B]): F[B] = compose((_:Unit) => ma, f)(())
   def map[A, B](ma: F[A])(f: A => B): F[B] = flatMap(ma)(a => unit(f(a)))
   def map2[A, B, C](ma: F[A], mb: F[B])(f: (A, B) => C): F[C] = flatMap(ma)(a => map(mb)(f(a, _)))
   /* -------- Exercise 11.3 ------- */
@@ -18,11 +19,19 @@ trait Monad[F[_]] extends Functor[F] {
   def replicateM[A](n: Int, ma: F[A]): F[List[A]] =
     if (n <= 0) unit(Nil)
     else map2(ma, replicateM(n - 1, ma))(_ :: _)
+  /* -------- Exercise 11.6 ------- */
+  def filterM[A](ms: List[A])(f: A => F[Boolean]): F[List[A]] =
+    ms.foldRight(unit(Nil:List[A]))((a,b) => map2(f(a),b)((res,acc) => if(res) acc else a :: acc))
+  /* -------- Exercise 11.7 ------- */
+  def compose[A,B,C](f: A => F[B], g: B => F[C]): A => F[C] = a => flatMap(f(a))(g)
 }
 
 object Monads {
   implicit class MonadSyntax[A,M[_]:Monad](ma: M[A]) {
+    def map[B](f: A => B): M[B] = implicitly[Monad[M]].map(ma)(f)
+    def map2[B,C](mb: M[B])(f: (A,B) => C): M[C] = implicitly[Monad[M]].map2(ma,mb)(f)
     def flatMap[B](f: A => M[B]): M[B] = implicitly[Monad[M]].flatMap(ma)(f)
+    def replicateM(n: Int): M[List[A]] = implicitly[Monad[M]].replicateM(n,ma)
   }
 
   /* -------- Exercise 11.1 ------- */
@@ -41,6 +50,4 @@ object Monads {
     override def unit[A](a: => A) = List(a)
     override def flatMap[A, B](ma: List[A])(f: A => List[B]) = ma flatMap f
   }
-
-
 }
