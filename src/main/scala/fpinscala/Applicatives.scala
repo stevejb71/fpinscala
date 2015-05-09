a1af942f6cbc9a665d516b542c63ee17d2a661f5
stevejb71 <stevejb71@yahoo.co.uk>
2015-05-09 17:48:36 +0800

    Add streamApplicative

--- src/main/scala/fpinscala/Applicatives.scala
+++ src/main/scala/fpinscala/Applicatives.scala
@@ -22,3 +22,10 @@ trait Applicative[F[_]] extends Functor[F] {
   def map3[A,B,C,D](fa: F[A], fb: F[B], fc: F[C])(f: (A,B,C) => D): F[D] = apply(apply(map(fa)(f.curried))(fb))(fc)
   def map4[A,B,C,D,E](fa: F[A], fb: F[B], fc: F[C], fd: F[D])(f: (A,B,C,D) => E): F[E] = apply(apply(apply(map(fa)(f.curried))(fb))(fc))(fd)
 }
+
+object Applicatives {
+  val streamApplicative = new Applicative[Stream] {
+    override def unit[A](a: => A): Stream[A] = Stream.continually(a)
+    override def map2[A, B, C](fa: Stream[A], fb: Stream[B])(f: (A, B) => C): Stream[C] = (fa zip fb) map f.tupled
+  }
+}
