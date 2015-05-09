8712cd9719d667876f32e912b3e81a5deecc79b5
stevejb71 <stevejb71@yahoo.co.uk>
2015-05-09 18:45:12 +0800

    add applicative laws

---
+++ src/test/scala/fpinscala/ApplicativeLaws.scala
@@ -0,0 +1,23 @@
+package fpinscala
+
+object Util {
+  implicit class TypeSafeEquals[A](val a1: A) extends AnyVal {
+    def ===(a2: A) = a1 == a2
+  }
+}
+
+import Util._
+
+object ApplicativeLaws {
+  def functorIdentity[M[_], A, B](F: Applicative[M], v: M[A]): Boolean = F.map(v)(identity) === v
+  def functorComposition[M[_], A, B, C](F: Applicative[M], v: M[A], f: B => C, g: A => B): Boolean = F.map(F.map(v)(g))(f) === F.map(v)(f compose g)
+
+  def leftIdentity[M[_], A](F: Applicative[M], fa: M[A]): Boolean = F.map2(F.unit(()), fa)((_,a) => a) === fa
+  def rightIdentity[M[_], A](F: Applicative[M], fa: M[A]): Boolean = F.map2(fa, F.unit(()))((a,_) => a) === fa
+
+  private def assoc[A,B,C](p: (A,(B,C))): ((A,B),C) = p match {
+    case (a,(b,c)) => ((a,b),c)
+  }
+  def associativity[M[_],A,B,C](F: Applicative[M], fa: M[A], fb: M[B], fc: M[C]): Boolean = F.product(F.product(fa,fb),fc) === F.map(F.product(fa,F.product(fb,fc)))(assoc)
+}
+
