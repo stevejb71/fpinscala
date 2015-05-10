6b75687e36bbdb5e037f81bb73c9189c5faa6875
stevejb71 <stevejb71@yahoo.co.uk>
2015-05-10 16:57:28 +0800

    Traverse

--- src/main/scala/fpinscala/Applicatives.scala
+++ src/main/scala/fpinscala/Applicatives.scala
@@ -34,14 +34,34 @@ trait Applicative[F[_]] extends Functor[F] {
   def compose[G[_]](G: Applicative[G]) = new Applicative[({type f[x] = F[G[x]]})#f] {
     private val base = Applicative.this
     override def unit[A](a: => A): F[G[A]] = base.unit(G.unit(a))
-    override def map2[A, B, C](fga: F[G[A]], fgb: F[G[B]])(f: (A, B) => C): F[G[C]] = base.map2(fga,fgb)((ga,gb) => G.map2(ga,gb)(f))
+    override def map2[A, B, C](fga: F[G[A]], fgb: F[G[B]])(f: (A, B) => C): F[G[C]] = base.map2(fga, fgb)((ga, gb) => G.map2(ga, gb)(f))
+  }
+
+  /* ------- Exercise 12.12 -------- */
+  def sequenceMap[K, V](ofa: Map[K, F[V]]): F[Map[K, V]] = ofa.foldLeft(unit(Map.empty[K, V])) {
+    case (m,(k,fv)) => map2(m, fv)((m,v) => m + (k -> v))
   }
 }
 
-
-object StreamApplicative extends Applicative[Stream] {
-  override def unit[A](a: => A): Stream[A] = Stream.continually(a)
-  override def map2[A, B, C](fa: Stream[A], fb: Stream[B])(f: (A, B) => C): Stream[C] = (fa zip fb) map f.tupled
+object Applicatives {
+  implicit val streamApplicative = new Applicative[Stream] {
+    override def unit[A](a: => A): Stream[A] = Stream.continually(a)
+    override def map2[A, B, C](fa: Stream[A], fb: Stream[B])(f: (A, B) => C): Stream[C] = (fa zip fb) map f.tupled
+  }
+  implicit val listApplicative = new Applicative[List] {
+    override def unit[A](a: => A): List[A] = List(a)
+    override def map2[A, B, C](fa: List[A], fb: List[B])(f: (A, B) => C): List[C] = for {
+      a <- fa
+      b <- fb
+    } yield f(a,b)
+  }
+  implicit val optionApplicative = new Applicative[Option] {
+    override def unit[A](a: => A): Option[A] = Some(a)
+    override def map2[A, B, C](fa: Option[A], fb: Option[B])(f: (A, B) => C): Option[C] = for {
+      a <- fa
+      b <- fb
+    } yield f(a,b)
+  }
 }
 
 /* ------- Exercise 12.6 -------- */
@@ -60,3 +80,35 @@ object Validation {
     }
   }
 }
+
+trait Traverse[F[_]] extends Functor[F] {
+  def traverse[G[_]: Applicative,A,B](fa: F[A])(f: A => G[B]): G[F[B]] = sequence(map(fa)(f))
+  def sequence[G[_]: Applicative,A](fga: F[G[A]]): G[F[A]] = traverse(fga)(identity)
+}
+
+case class Tree[+A](head: A, tail: List[Tree[A]])
+
+/* ------- Exercise 12.13 -------- */
+object Traverse {
+  val listTraverse = new Traverse[List] {
+    override def map[A, B](fa: List[A])(f: A => B): List[B] = fa map f
+    override def sequence[G[_] : Applicative, A](fga: List[G[A]]): G[List[A]] = {
+      val GA = implicitly[Applicative[G]]
+      fga.foldRight(GA.unit(Nil:List[A]))((ga,gas) => GA.map2(ga,gas)(_ :: _))
+    }
+  }
+
+  val optionTraverse = new Traverse[Option] {
+    override def map[A, B](fa: Option[A])(f: A => B): Option[B] = fa map f
+    override def sequence[G[_] : Applicative, A](fga: Option[G[A]]): G[Option[A]] = {
+      val GA = implicitly[Applicative[G]]
+      fga.fold(GA.unit(None:Option[A]))(ga => GA.map(ga)(Some(_)))
+    }
+  }
+
+  val treeTraverse = new Traverse[Tree] {
+    override def map[A, B](fa: Tree[A])(f: A => B): Tree[B] = Tree(f(fa.head),fa.tail map f)
+    override def sequence[G[_] : Applicative, A](fga: Tree[G[A]]): G[Tree[A]] = ???
+  }
+}
+
